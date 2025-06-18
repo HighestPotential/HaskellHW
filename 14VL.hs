d27fd1d2c1cd35acbc0620c2545a4250e7e5d796
HighestPotential <dm4tro.d@gmail.com>
2025-06-18 11:06:40 +0200

    Notes for 14VL

--- 14VL.hs
+++ 14VL.hs
@@ -1,12 +1,18 @@
+import Data.Char
+
 main :: IO ()
-main= do
-    putStrLn "what is your name?"
-    neme <- getLine
-    putStrLn ("Hello " ++ neme)
+main = interact processInput
+    where
+        processInput = (map toUpper) . shortLines 
+
+shortLines :: String -> String
+shortLines = unlines . shortfilter . lines
+    where
+    shortfilter = filter (\line -> length line <= 10)
 
 hello2 :: IO String
 hello2 = do
-    () <- putStrLn "Ihr Name bitte: "
+    putStrLn "Ihr Name bitte: "
     name <- getLine
     _ <- putStrLn ("Sie heissen " ++ name ++ "?")
     getLine
@@ -15,5 +21,23 @@ hello :: IO ()
 hello = do
     name <- getLine
     putStrLn ("Hello " ++ " ")
-    
-   
+
+
+hello3 :: IO String
+hello3 = do
+    putStrLn "Ihr Name bitte: "
+    name <- getLine
+    putStrLn $ "Sie heissen " ++ name ++ "?" ++ sicher
+    getLine
+  where
+    sicher = '\n' : "Sind Sie sich sicher?"
+
+
+aktion1 :: IO ()
+aktion1 = putStrLn "Hallo"
+
+aktion2 :: IO ()
+aktion2 = putStrLn "Welt"
+
+main' :: IO ()
+main' = aktion1 >> aktion2
